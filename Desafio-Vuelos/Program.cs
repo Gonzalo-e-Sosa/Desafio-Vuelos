fe865be3e7003c985f49a81093cd373dcb6b505e
Gonza <gonzalososa342@gmail.com>
2024-08-02 22:46:48 -0300

    Added flights route to get all flights of the sample data

--- Desafio-Vuelos/Program.cs
+++ Desafio-Vuelos/Program.cs
@@ -1,6 +1,10 @@
+using Desafio_Vuelos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/flights", () => new Repository().GetFlights());
 
 app.Run();
