51a51179e7dc88a7030c640237a21aee124c7d13
Gonza <gonzalososa342@gmail.com>
2024-08-02 22:28:09 -0300

    Added list of flights with sample data to Repository. Added required to FlightNumber and Airline in Flight class.

--- Desafio-Vuelos/Repository.cs
+++ Desafio-Vuelos/Repository.cs
@@ -2,14 +2,36 @@
 {
 	public class Repository
 	{
+		List<Flight> flights = new List<Flight>()
+		{
+			new() {
+				FlightNumber="AU2401",
+				ArrivalTime= new DateTime(),
+				Airline="Austral Lineas Aereas",
+				Delayed=true
+			},
+			new(){
+				FlightNumber="4M5994",
+				ArrivalTime= new DateTime(),
+				Airline="Latam Argentina",
+				Delayed=false
+			},
+			new(){
+				FlightNumber="AR2939",
+				ArrivalTime= new DateTime(),
+				Airline="Aerolineas Argentinas",
+				Delayed=false
+			}
+		};
 
+		public List<Flight> GetFlights() => flights; 
 	}
 
 	public class Flight
 	{
-		public string FlightNumber { get; set; }
+		public required string FlightNumber { get; set; }
 		public DateTime ArrivalTime { get; set; }
-		public string Airline { get; set; }
+		public required string Airline { get; set; }
 		public bool Delayed { get; set; }
 	}
 }
