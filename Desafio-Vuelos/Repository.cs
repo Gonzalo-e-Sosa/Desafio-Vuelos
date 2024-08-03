0abe365f8190c7decb30ef821160a6ca99366c4f
Gonza <gonzalososa342@gmail.com>
2024-08-02 22:17:07 -0300

    Added Repository file with Flight class

---
+++ Desafio-Vuelos/Repository.cs
@@ -0,0 +1,15 @@
+﻿namespace Desafio_Vuelos
+{
+	public class Repository
+	{
+
+	}
+
+	public class Flight
+	{
+		public string FlightNumber { get; set; }
+		public DateTime ArrivalTime { get; set; }
+		public string Airline { get; set; }
+		public bool Delayed { get; set; }
+	}
+}
